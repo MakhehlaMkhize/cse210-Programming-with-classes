256314d7991a02ab391f95175404d4b50a6068e2
MakhehlaMkhize <mkh22001@byui.edu>
2023-11-22 07:28:16 +0200

    Week 4 inheritance learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new("Makhehla", "Mathematics" );
+        string display = assignment.GetSummary();
+        MathAssignment mathAssignment = new("Makhehla Mkhize","Mathematics","section 7.3", "Problem 8-19");
+        WritingAssignment writingAssignment = new("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
